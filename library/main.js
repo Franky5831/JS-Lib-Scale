e3b4a49dccfc7baccf5dd8c4a4b9d35524262b6e
Francesco Ciannavei <fracianna5@gmail.com>
2025-05-10 16:19:38 +0200

    basic functionality

---
+++ library/main.js
@@ -0,0 +1,50 @@
+class ButtonScale {
+
+	constructor(buttonSelector, parameters) {
+		this.buttonSelector = buttonSelector;
+		this.parameters = parameters;
+		this.hoverScale = this.parameters.hoverScale || false;
+		this.clickScale = this.parameters.clickScale || false;
+
+		this.init()
+	}
+
+	init(){
+		let elements = this.getElements();
+
+		for(let i = 0; i < elements.length; i++){
+			let element = elements[i];
+
+			if(this.hoverScale){
+				element.addEventListener("mouseover", () => {
+					let scale = this.calculateScale(element.offsetWidth, this.hoverScale);
+					element.style.transform = `scale(${scale})`;
+				});
+				element.addEventListener("mouseleave", () => {
+					element.style.transform = `scale(1)`;
+				});
+			}
+
+			if(this.clickScale){
+				element.addEventListener("mousedown", () => {
+					let scale = this.calculateScale(element.offsetWidth, this.clickScale);
+					element.style.transform = `scale(${scale})`;
+				});
+				element.addEventListener("mouseup", () => {
+					let scale = this.calculateScale(element.offsetWidth, this.hoverScale);
+					element.style.transform = `scale(${scale})`;
+				});
+			}
+		}
+	}
+
+	calculateScale(elementWidth, transformScale){
+		let originalWidth = elementWidth;
+		let updatedWidth = elementWidth + transformScale;
+		return (updatedWidth / originalWidth).toFixed(2);
+	}
+
+	getElements() {
+		return document.querySelectorAll(this.buttonSelector);
+	}
+}
