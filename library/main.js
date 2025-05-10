a1d1097cffdb56d6fb75dcd568f0b24cd94ffbfb
Francesco Ciannavei <fracianna5@gmail.com>
2025-05-10 23:43:53 +0200

    light refactor

--- library/main.js
+++ library/main.js
@@ -1,5 +1,4 @@
 class ButtonScale {
-
 	constructor(buttonSelector, parameters) {
 		this.buttonSelector = buttonSelector;
 		this.parameters = parameters;
@@ -9,36 +8,38 @@ class ButtonScale {
 		this.init()
 	}
 
-	init(){
+	init() {
 		let elements = this.getElements();
 
-		for(let i = 0; i < elements.length; i++){
+		for (let i = 0; i < elements.length; i++) {
 			let element = elements[i];
-
-			if(this.hoverScale){
-				element.addEventListener("mouseover", () => {
-					let scale = this.calculateScale(element.offsetWidth, this.hoverScale);
-					element.style.transform = `scale(${scale})`;
-				});
-				element.addEventListener("mouseleave", () => {
-					element.style.transform = `scale(1)`;
-				});
-			}
-
-			if(this.clickScale){
-				element.addEventListener("mousedown", () => {
-					let scale = this.calculateScale(element.offsetWidth, this.clickScale);
-					element.style.transform = `scale(${scale})`;
-				});
-				element.addEventListener("mouseup", () => {
-					let scale = this.calculateScale(element.offsetWidth, this.hoverScale);
-					element.style.transform = `scale(${scale})`;
-				});
-			}
+			if (this.hoverScale) this.initHoverScale(element)
+			if (this.clickScale) this.initMouseDownScale(element);
 		}
 	}
 
-	calculateScale(elementWidth, transformScale){
+	initHoverScale(element) {
+		element.addEventListener("mouseover", () => {
+			let scale = this.calculateScale(element.offsetWidth, this.hoverScale);
+			element.style.transform = `scale(${scale})`;
+		});
+		element.addEventListener("mouseleave", () => {
+			element.style.transform = `scale(1)`;
+		});
+	}
+
+	initMouseDownScale(element) {
+		element.addEventListener("mousedown", () => {
+			let scale = this.calculateScale(element.offsetWidth, this.clickScale);
+			element.style.transform = `scale(${scale})`;
+		});
+		element.addEventListener("mouseup", () => {
+			let scale = this.calculateScale(element.offsetWidth, this.hoverScale);
+			element.style.transform = `scale(${scale})`;
+		});
+	}
+
+	calculateScale(elementWidth, transformScale) {
 		let originalWidth = elementWidth;
 		let updatedWidth = elementWidth + transformScale;
 		return (updatedWidth / originalWidth).toFixed(2);
